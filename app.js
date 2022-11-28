655b0a21fb1e3e142fb55324ebb93a2182b40491
arifvtti <arif.vtti@gmail.com>
2022-11-28 11:21:15 +0600

    login and register route added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+
 
 require("./config/connectDB");
+const userRouter = require("./routes/user.route");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user", userRouter);
 
 // base url
 app.get("/", (req, res) => {
---
+++ controllers/user.controller.js
@@ -0,0 +1,104 @@
+const User = require("../models/user.model")
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
+
+exports.registerUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(401).json({
+                success: false,
+                message: "Username and password is required"
+            })
+        }
+
+        const exist = await User.findOne({ username });
+
+        if (exist) {
+            return res.status(401).json({
+                success: false,
+                message: "Username not available"
+            })
+        }
+
+        bcrypt.hash(password, salt, async (err, hash) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: "Internal server error"
+                })
+            }
+            const user = new User({
+                username,
+                password: hash
+            })
+            await user.save();
+            res.status(201).json({
+                success: true,
+                message: "Successfully registered user",
+                user: {
+                    id: user._id,
+                    username: user.username
+                }
+            })
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't register the user"
+        })
+    }
+}
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(401).json({
+                success: false,
+                message: "Username and password is required"
+            })
+        }
+
+        const exist = await User.findOne({ username });
+
+        if (!exist) {
+            return res.status(401).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        const { password: pwd, ...user } = exist.toObject();
+        console.log(exist)
+
+        bcrypt.compare(password, exist.password, async (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: "Internal server error"
+                })
+            }
+            if (!result) {
+                return res.status(500).json({
+                    success: false,
+                    message: "Username or password invalid"
+                })
+            }
+
+            res.status(200).json({
+                success: true,
+                message: "Successfully logged in user",
+                user
+            })
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't logged in the user"
+        })
+    }
+}
